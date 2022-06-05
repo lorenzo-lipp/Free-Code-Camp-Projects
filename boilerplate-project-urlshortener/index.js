631183a812d4552e5560187b083727f1f687dda7
lorenzo-lipp <91420499+lorenzo-lipp@users.noreply.github.com>
2022-06-05 11:41:59 -0300

    Update index.js

--- boilerplate-project-urlshortener/index.js
+++ boilerplate-project-urlshortener/index.js
@@ -3,7 +3,14 @@ const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const shortUrls = []
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const { Schema } = mongoose
+const URLSchema = new Schema({
+  "original_url": {type: String, required: true},
+  "short_url": {type: Number, required: true}
+})
+const URL = mongoose.model('URL', URLSchema)
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -23,26 +30,30 @@ app.get('/api/hello', function(req, res) {
 
 app.post('/api/shorturl', (req, res) => {
   console.log(req.body.url)
-  const short = shortUrls.length + 1
-  const url = req.body.url
-  const checkURL = url.match(/https?:\/\/[a-zA-Z0-9-_]*\..*/)
-  const jsonResponse = { "original_url": url, "short_url": short }
-  if (url && checkURL) {
-    shortUrls.push(jsonResponse)
-    res.json(jsonResponse)
-  } else {
-    res.json({ "error": 'invalid url' })
-  }
+  URL.countDocuments({})
+    .then((count) => {
+      const short = count + 1
+      const url = req.body.url
+      const checkURL = url.match(/https?:\/\/[a-zA-Z0-9-_]*\..*/)
+      const jsonResponse = { "original_url": url, "short_url": short }
+      if (url && checkURL) {
+        const shortUrl = new URL(jsonResponse)
+        shortUrl.save()
+          .then(() => res.json(jsonResponse))
+          .catch((err) => res.json(err))
+      } else {
+        res.json({ "error": 'invalid url' })
+      }
+    })
+    .catch((err) => res.json(err))
 })
 
 app.get('/api/shorturl/:num', (req, res) => {
   console.log(req.params.num)
-  const url = shortUrls.find(v => v["short_url"] == req.params.num)
-  if (url) {
-    res.redirect(url["original_url"])
-  } else {
-    res.json({ "error": 'invalid url' })
-  }
+  console.log(URL.findOne({"short_url": req.params.num}))
+  URL.findOne({"short_url": req.params.num})
+    .then((url) => res.redirect(url["original_url"]))
+    .catch((err) => res.json(err))
 })
 
 app.listen(port, function() {
