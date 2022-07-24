ec2fdc50c01975bf13a35a64b94eb5edcdcc5d26
lorenzo-lipp <lorenzo1lipp@gmail.com>
2022-07-24 20:53:51 -0300

    Pairwise algorithm added

--- algorithms/pairwise/pairwise.js
+++ algorithms/pairwise/pairwise.js
@@ -1,14 +1,13 @@
 function pairwise(arr, arg) {
     let indicesSum = 0;
-    let used = [];
 
     for (let i in arr) {
         for (let j in arr) {
             if (j <= i) continue;
-            if (j in used || i in used) continue;
             if (arr[i] + arr[j] === arg) {
                 indicesSum += (+i) + (+j);
-                used.push(i, j);
+                arr[i] = NaN;
+                arr[j] = NaN;
             }
         }
     }
