ce6c0cb8ff57cfc6e89c7ce672d4ad0f423ad653
lorenzo-lipp <lorenzo1lipp@gmail.com>
2022-07-25 19:29:23 -0300

    Quick Sort algorithm added

---
+++ algorithms/quick-sort/quick-sort.js
@@ -0,0 +1,29 @@
+function quickSort(array) {
+    let pivot = array[0];
+    let biggerThanPivot = [];
+    let smallerThanPivot = [];
+    let equalToPivot = [];
+
+    for (let i of array) {
+        if (i < pivot) {
+            smallerThanPivot.push(i);
+        } else if (i > pivot) {
+            biggerThanPivot.push(i);
+        } else {
+            equalToPivot.push(i);
+        }
+    }
+
+    if (smallerThanPivot.length > 1) {
+        smallerThanPivot = quickSort(smallerThanPivot);
+    }
+    if (biggerThanPivot.length > 1) {
+        biggerThanPivot = quickSort(biggerThanPivot);
+    }
+
+    array = smallerThanPivot.concat(equalToPivot).concat(biggerThanPivot);
+
+    return array;
+}
+
+quickSort([5,26,1,2,3,1])
