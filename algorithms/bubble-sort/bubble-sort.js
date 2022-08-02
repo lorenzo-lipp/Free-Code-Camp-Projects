a8a786e04d13c8b0be8d0a4c70820732631b68f0
lorenzo-lipp <lorenzo1lipp@gmail.com>
2022-08-02 19:49:51 -0300

    Bubble sort improved

--- algorithms/bubble-sort/bubble-sort.js
+++ algorithms/bubble-sort/bubble-sort.js
@@ -1,11 +1,8 @@
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] > array[i + 1]) {
-            let temp = [array[i], array[i + 1]];
-            array[i + 1] = temp[0];
-            array[i] = temp[1];
+            [array[i + 1], array[i]] = [array[i], array[i + 1]];
             i = -1;
-            continue;
         }
     }
     
