98a27a6fe3fc6d30d4e4dfa72ae648c144885098
lorenzo-lipp <lorenzo1lipp@gmail.com>
2022-08-02 19:54:39 -0300

    Insertion sort improved

--- algorithms/insertion-sort/insertion-sort.js
+++ algorithms/insertion-sort/insertion-sort.js
@@ -1,11 +1,10 @@
 function insertionSort(array) {
-    loopArray:
     for (let i = 1; i < array.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if (array[i] < array[j]) {
-                array = [...array.slice(0, j), array[i], ...array.slice(j, i), ...array.slice(i + 1)];
-                continue loopArray;
+        for (let j = i; j > 0; j--) {
+            if (array[j - 1] <= array[j]) {
+                break;
             }
+            [array[j], array[j - 1]] = [array[j - 1], array[j]];
         }
     }
 
